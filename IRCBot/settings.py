06ab37f830218d474b5b100d4073e51b10128166
Themistokle Benetatos <info@mrt-prodz.com>
2016-05-16 14:36:04 +0300

    bot configuration settings

---
+++ IRCBot/settings.py
@@ -0,0 +1,17 @@
+import logging
+
+loglevel = logging.ERROR
+config = [
+            {
+                'host'    :'10.0.0.10',
+                'port'    : 6697,
+                'ssl'     : True,
+                'nick'    : 'ctfbot',
+                'chans'   : ['#ctfbot'],
+                'pwd'     : 'nickservbotpassword',
+                'salt'    : 'Salty Cheesy Poofs!',
+                'admin'   : 'df9947089cd1895cce41cffb4dc8388ff11aa134b3f16d6587e306ed33882060',
+                'enabled' : True,
+            },
+        ]
+
